056dc501f8c64a732737879c8c80829fced8550d
James Schonian <jmschonian@utexas.edu>
2014-09-14 03:53:20 -0500

    Added Constants and Models
    -Added constants that correspond to the name field of the json response
    from the csgo stats api
    -Added a model to hold the csgo stats data once retrieved from the api

---
+++ GlobalOffensiveStats/Constants/ApiConstants.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalOffensiveStats.Constants
+{
+    class GetUserStatsForGameConstants
+    {
+        public const String TOTAL_KILLS = "total_kills";
+        public const String TOTAL_DEATHS = "total_deaths";
+        public const String TOTAL_WINS = "total_wins";
+        public const String TOTAL_DAMAGE_DONE = "total_damage_done";
+        public const String TOTAL_MONEY_EARNED = "total_money_earned";
+        public const String TOTAL_HEADSHOT_KILLS = "total_kills_headshot";
+        public const String TOTAL_SHOTS_FIRED = "total_shots_fired";
+        public const String TOTAL_SHOTS_HIT = "total_shots_hit";
+        public const String TOTAL_ROUNDS_PLAYED = "total_rounds_played";
+        public const String TOTAL_MVPS = "total_mvps";
+        public const String ALL_TIME_ACCURACY = "total_kills";
+    }
+}
---
+++ GlobalOffensiveStats/Models/CsgoStats.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+public class CsgoStatsModel
+{
+
+    private int totalKills;
+    private int totalDeaths;
+    private int totalWins;
+    private int totalDamageDone;
+    private int totalMoneyEarned;
+    private int totalHeadshotKills;
+    private int totalShotsFired;
+    private int totalShotsHit;
+    private int totalRoundsPlayed;
+    private int totalMvps;
+
+
+
+    public CsgoStatsModel()
+    {
+    }
+
+    public void setTotalKills(int totalKills)
+    {
+        this.totalKills = totalKills;
+    }
+
+    public int getTotalKills()
+    {
+        return totalKills;
+    }
+
+    public void setTotalDeaths(int totalDeaths)
+    {
+        this.totalDeaths = totalDeaths;
+    }
+
+    public int getTotalDeaths()
+    {
+        return totalDeaths;
+    }
+
+    public void setTotalWins(int totalWins)
+    {
+        this.totalWins = totalWins;
+    }
+
+    public int getTotalWins()
+    {
+        return totalWins;
+    }
+
+    public void setTotalDamageDone(int totalDamageDone)
+    {
+        this.totalDamageDone = totalDamageDone;
+    }
+
+    public int getTotalDamageDone()
+    {
+        return totalDamageDone;
+    }
+
+    public void setTotalMoneyEarned(int totalDamageDone)
+    {
+        this.totalDamageDone = totalDamageDone;
+    }
+
+    public int getTotalDamageDone()
+    {
+        return totalDamageDone;
+    }
+}
