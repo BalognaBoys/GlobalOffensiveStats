3383aa74bda6188a4be2765b3918717972146547
jbbillon <jbbillon@gmail.com>
2014-09-13 21:14:36 -0700

    Made Project Folder

---
+++ GlobalOffensiveStats/System.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GlobalOffensiveStats
+{
+    class Application
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Counter strike lol");
+            System.Console.ReadKey();
+        }
+ 
+    }
+}
